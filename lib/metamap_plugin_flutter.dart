4159b841c71a0df2310ea53e4237c80f8f6a0a01
PaoloTolentino <tolentinopaolomatthew23@gmail.com>
2023-03-27 13:00:30 +0800

    hotfix: fixed dynamic type for resultCompleter

--- lib/metamap_plugin_flutter.dart
+++ lib/metamap_plugin_flutter.dart
@@ -2,16 +2,17 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class MetaMapFlutter {
-  static var resultCompleter;
+  static late final Completer<Result> resultCompleter;
 
   static const MethodChannel _channel = const MethodChannel('mati_flutter');
 
-  static Future<String> showMetaMapFlow(String clientId, String flowId, Map<String, dynamic> metadata) async {
-  _channel.setMethodCallHandler(handler);
-  resultCompleter = Completer<Result>();
-  metadata["sdkType"] = "flutter";
+  static Future<String> showMetaMapFlow(
+      String clientId, String flowId, Map<String, dynamic> metadata) async {
+    _channel.setMethodCallHandler(handler);
+    resultCompleter = Completer<Result>();
+    metadata["sdkType"] = "flutter";
 
-  return await _channel.invokeMethod('showMatiFlow', <String, dynamic> {
+    return await _channel.invokeMethod('showMatiFlow', <String, dynamic>{
       'clientId': clientId,
       'flowId': flowId,
       'metadata': metadata,
@@ -21,24 +22,22 @@ class MetaMapFlutter {
   static Future<Result?> handler(MethodCall call) async {
     switch (call.method) {
       case "cancelled":
-          resultCompleter.complete(ResultCancelled());
-          return null;
+        resultCompleter.complete(ResultCancelled());
+        return null;
       case "success":
-          String text = call.arguments;
-          List<String> result = text.split(' ');
-          String verificationId = result[0];
-          String identityId = result[1];
-          resultCompleter.complete(ResultSuccess(verificationId, identityId));
-          return null;
+        String text = call.arguments;
+        List<String> result = text.split(' ');
+        String verificationId = result[0];
+        String identityId = result[1];
+        resultCompleter.complete(ResultSuccess(verificationId, identityId));
+        return null;
       default:
         throw MissingPluginException('notImplemented');
     }
   }
 }
 
-abstract class Result {
-
-}
+abstract class Result {}
 
 class ResultSuccess extends Result {
   final String verificationId;
@@ -46,6 +45,4 @@ class ResultSuccess extends Result {
   ResultSuccess(this.verificationId, this.identityId);
 }
 
-class ResultCancelled extends Result {
-
-}
+class ResultCancelled extends Result {}
